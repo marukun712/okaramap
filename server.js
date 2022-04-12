b7bc66cecb96de2c4acb3b2d339d7c9f9f097459
marukun_ <87289638+marukun712@users.noreply.github.com>
2022-04-12 09:24:35 +0900

    server

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 var express = require("express");
 var app = express();
 var port = 3000
-  
 
-app.get('/',function(req,res){
-    app.use(express.static('.'))
-    res.sendFile(__dirname+'/src/index.html')
-    
+
+app.get('/', function(req, res) {
+    app.use(express.static('./src'))
+    res.sendFile(__dirname + '/src/index.html')
+
 })
 
 
-app.listen(port,function(){
-    console.log("http://localhost:"+port+"で起動")
+app.listen(port, function() {
+    console.log("http://localhost:" + port + "で起動")
 })
-  
-
--- src/index.js
+++ src/index.js
@@ -1,118 +1,118 @@
 var member = {
-  'uno': {
-    id: "UCLfAsY3iMUAF2vvDxvIjymQ"
-  },
-  'itsuki': {
-    id: "UCi3aT9cC6YyC0BnOpm2XBEw"
-  },
-  'otoha': {
-    id: "UCYcnLc0n1ryBDZeGWQTVJ_g"
-  },
-  'sera': {
-    id: "UCvXsXmpMKthJuX8XHbsnOjQ"
-  },
-  'maru': {
-    id: "UCOg01LJmZF9UnwFbly73CVw"
-  }
+    'uno': {
+        id: "UCLfAsY3iMUAF2vvDxvIjymQ"
+    },
+    'itsuki': {
+        id: "UCi3aT9cC6YyC0BnOpm2XBEw"
+    },
+    'otoha': {
+        id: "UCYcnLc0n1ryBDZeGWQTVJ_g"
+    },
+    'sera': {
+        id: "UCvXsXmpMKthJuX8XHbsnOjQ"
+    },
+    'maru': {
+        id: "UCOg01LJmZF9UnwFbly73CVw"
+    }
 }
 var ids = []
 $.ajax({
-  type: 'GET',
-  url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + member.itsuki.id + "&key=" + key2 + "&eventType=upcoming&type=video",
-  datatype: 'json',
-  success: function (json) {
-    num = json.items.length;
-    for (let i = 0; i < num; i++) {
-      var ID = json.items[i].id.videoId
-      ids.push(json.items[i].id.videoId);
-      var title = json.items[i].snippet.title
-      var image = json.items[i].snippet.thumbnails.medium.url;
-      $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">💙${title}</a></td></tr>`);
-      $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
+    type: 'GET',
+    url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + member.itsuki.id + "&key=" + key + "&eventType=upcoming&type=video",
+    datatype: 'json',
+    success: function(json) {
+        num = json.items.length;
+        for (let i = 0; i < num; i++) {
+            var ID = json.items[i].id.videoId
+            ids.push(json.items[i].id.videoId);
+            var title = json.items[i].snippet.title
+            var image = json.items[i].snippet.thumbnails.medium.url;
+            $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">💙${title}</a></td></tr>`);
+            $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
+        }
     }
-  }
 })
 
 $.ajax({
-  type: 'GET',
-  url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + member.uno.id + "&key=" + key2 + "&eventType=upcoming&type=video",
-  datatype: 'json',
-  success: function (json) {
-    num = json.items.length;
-    for (let i = 0; i < num; i++) {
-      var ID = json.items[i].id.videoId
-      ids.push(json.items[i].id.videoId);
-      var title = json.items[i].snippet.title
-      var image = json.items[i].snippet.thumbnails.medium.url;
-      $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">🧡${title}</a></td></tr>`);
-      $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
+    type: 'GET',
+    url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + member.uno.id + "&key=" + key + "&eventType=upcoming&type=video",
+    datatype: 'json',
+    success: function(json) {
+        num = json.items.length;
+        for (let i = 0; i < num; i++) {
+            var ID = json.items[i].id.videoId
+            ids.push(json.items[i].id.videoId);
+            var title = json.items[i].snippet.title
+            var image = json.items[i].snippet.thumbnails.medium.url;
+            $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">🧡${title}</a></td></tr>`);
+            $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
+        }
     }
-  }
 })
 $.ajax({
-  type: 'GET',
-  url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + member.maru.id + "&key=" + key2 + "&eventType=upcoming&type=video",
-  datatype: 'json',
-  success: function (json) {
-    num = json.items.length;
-    for (let i = 0; i < num; i++) {
-      var ID = json.items[i].id.videoId
-      ids.push(json.items[i].id.videoId);
-      var title = json.items[i].snippet.title
-      var image = json.items[i].snippet.thumbnails.medium.url;
-      $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">💛${title}</a></td></tr>`);
-      $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
+    type: 'GET',
+    url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + member.maru.id + "&key=" + key + "&eventType=upcoming&type=video",
+    datatype: 'json',
+    success: function(json) {
+        num = json.items.length;
+        for (let i = 0; i < num; i++) {
+            var ID = json.items[i].id.videoId
+            ids.push(json.items[i].id.videoId);
+            var title = json.items[i].snippet.title
+            var image = json.items[i].snippet.thumbnails.medium.url;
+            $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">💛${title}</a></td></tr>`);
+            $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
+        }
     }
-  }
 })
 $.ajax({
-  type: 'GET',
-  url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + member.sera.id + "&key=" + key2 + "&eventType=upcoming&type=video",
-  datatype: 'json',
-  success: function (json) {
-    num = json.items.length;
-    for (let i = 0; i < num; i++) {
-      var ID = json.items[i].id.videoId
-      ids.push(json.items[i].id.videoId);
-      var title = json.items[i].snippet.title
-      var image = json.items[i].snippet.thumbnails.medium.url;
-      $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">❤${title}</a></td></tr>`);
-      $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
+    type: 'GET',
+    url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + member.sera.id + "&key=" + key + "&eventType=upcoming&type=video",
+    datatype: 'json',
+    success: function(json) {
+        num = json.items.length;
+        for (let i = 0; i < num; i++) {
+            var ID = json.items[i].id.videoId
+            ids.push(json.items[i].id.videoId);
+            var title = json.items[i].snippet.title
+            var image = json.items[i].snippet.thumbnails.medium.url;
+            $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">❤${title}</a></td></tr>`);
+            $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
+        }
     }
-  }
 })
 $.ajax({
-  type: 'GET',
-  url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + member.otoha.id + "&key=" + key2 + "&eventType=upcoming&type=video",
-  datatype: 'json',
-  success: function (json) {
-    num = json.items.length;
-    for (let i = 0; i < num; i++) {
-      var ID = json.items[i].id.videoId
-      ids.push(json.items[i].id.videoId);
-      var title = json.items[i].snippet.title
-      var image = json.items[i].snippet.thumbnails.medium.url;
-      $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">💜${title}</a></td></tr>`);
-      $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
+    type: 'GET',
+    url: "https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=" + member.otoha.id + "&key=" + key + "&eventType=upcoming&type=video",
+    datatype: 'json',
+    success: function(json) {
+        num = json.items.length;
+        for (let i = 0; i < num; i++) {
+            var ID = json.items[i].id.videoId
+            ids.push(json.items[i].id.videoId);
+            var title = json.items[i].snippet.title
+            var image = json.items[i].snippet.thumbnails.medium.url;
+            $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">💜${title}</a></td></tr>`);
+            $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
+        }
     }
-  }
 })
 
 function gettime() {
-  console.log(ids)
-  num = ids.length;
-  for (let i = 0; i < num; i++) {
-    $.ajax({
-      type: 'GET',
-      url: `https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=${ids}&key=${key2}`,
-      datatype: 'json',
-      success: function (json) {
-        num = json.items.length;
-        var timedata = json.items[i].liveStreamingDetails.scheduledStartTime
-        var time = moment(timedata).format('YYYY.MM.DD HH:mm')
-        $("#time").append(`<tr><td class="border">${time}</td></tr>`)
-      }
-    })
-  }
+    console.log(ids)
+    num = ids.length;
+    for (let i = 0; i < num; i++) {
+        $.ajax({
+            type: 'GET',
+            url: `https://www.googleapis.com/youtube/v3/videos?part=liveStreamingDetails&id=${ids}&key=${key}`,
+            datatype: 'json',
+            success: function(json) {
+                num = json.items.length;
+                var timedata = json.items[i].liveStreamingDetails.scheduledStartTime
+                var time = moment(timedata).format('YYYY.MM.DD HH:mm')
+                $("#time").append(`<tr><td class="border">${time}</td></tr>`)
+            }
+        })
+    }
 }
 setTimeout(gettime, 1000)
