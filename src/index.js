feeee253ad8cce43203a90d0dee1b9e0f078bc2a
marukun_ <marukun530@gmail.com>
2023-09-05 15:05:50 +0900

    ⚡️ APIのURLを変更

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-fetch('https://marukun-dev.com/youtube/verse')
+fetch('https://marukun-dev.com/verse/api')
     .then(response => {
         return response.json();
     })
@@ -6,10 +6,10 @@ fetch('https://marukun-dev.com/youtube/verse')
         console.log(data);
         var num = data.length
         for (let i = 0; i < num; i++) {
-            var title = data[i].items.title
-            var ID = data[i].items.id
-            var time = data[i].items.time
-            var image = data[i].items.image
+            var title = data[i].title
+            var ID = data[i].id
+            var time = data[i].time
+            var image = data[i].image
 
             $("#main").append(`<tr><td class="border"><a href="https://www.youtube.com/watch?v=${ID}">${title}</a></td></tr>`);
             $("#image").append(`<tr><td><a href="https://www.youtube.com/watch?v=${ID}"><img src="${image}" style="border: 9px solid white;"></a></td></tr>`);
